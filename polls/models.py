06be6cb217affb132435a3da3ab2bd10ab42cb16
webdessin <webdessin@naver.com>
2021-07-15 16:43:07 +0900

    django

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=100)
-    public_date = models.DateTimeField()
-    votes = models.BigIntegerField()
+class Question(models.Model):   # Table
+    question_text = models.CharField(max_length=100)    # columm, datatype
+    public_date = models.CharField(max_length=100)
+    votes = models.DecimalField(max_digits=20, decimal_places=10)
+
+class Economics(models.Model):
+    title = models.CharField(max_length=100)
+    href = models.CharField(max_length=100)
+    create_date = models.CharField(max_length=100)
